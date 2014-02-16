f870a9b03aae7e02866446a44ffae0ff358a3f0b
git2000 <git2000@local>
2014-02-15 23:16:00 -0500

    rc

--- src/FileMD5.java
+++ src/FileMD5.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FilenameUtils;
 
